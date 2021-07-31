29f67283969d0d79ce2ba6f169545bdfd75c1929
Augustin.B <augustinbielefeld@gmail.com>
2021-07-31 15:50:32 +0200

    TMC2209_Lib: add basic class code

---
+++ lib/TMC2209_Lib/TMC2209_Lib.cpp
@@ -0,0 +1,72 @@
+/*
+ * TMC2209_Lib.cpp
+ *
+ *  Created on: 13.07.2021
+ *      Author: AB
+ */
+
+#include "TMC2209_Lib.hpp"
+
+#define TMC2209_VERSION_BYTE    0x40  /* Chip version. */
+
+TMC2209::TMC2209(HardwareSerial& serialPort, uint32_t baudrate, uint8_t chipAddress, uint8_t enablePin)
+    : TMCSerial(serialPort, baudrate, chipAddress), _enablePin(enablePin), _isConfigured(false)
+{
+
+}
+
+bool TMC2209::begin()
+{
+    /* Call base class begin() to set up UART peripheral. */
+    TMCSerial::begin();
+
+    /* Check that chip is alive. */
+    if (isChipAlive() != true)
+        return false;
+
+    /* Set up enable pin. */
+    pinMode(_enablePin, OUTPUT);
+
+    /* Disable bridges. */
+    digitalWrite(_enablePin, LOW);
+
+    /* Write fundamental parameters. */
+
+
+    return true;
+}
+
+bool TMC2209::isChipAlive()
+{
+    if (_isConfigured != true)
+    {
+        /* If driver has not been configured, first check that we can read registers. */
+        TMCSerial::enableReadChecksum(true);
+        int8_t serialErr;
+
+        if ((TMCSerial::readField(TMC22XX_VERSION, serialErr) != TMC2209_VERSION_BYTE) || serialErr < 0)
+        {
+            /* If we have the wrong chip, or a checksum error, fail here. */
+            return false;
+        }
+
+        /* If checksum just passed, chances are it should be fine most of the time.
+         * Disable here to save on computing. */
+        TMCSerial::enableReadChecksum(false);
+    }
+
+    /* If this passes, we check some more parameters. */
+    if (TMCSerial::readField(TMC22XX_DRV_ERR) == true)
+    {
+        /* Overtemperature or short circuit protection was tripped, yikes! */
+        return false;
+    }
+
+    if (TMCSerial::readField(TMC22XX_UV_CP) == true)
+    {
+        /* Undervoltage on charge pump input, this disables the driver. */
+        return false;
+    }
+
+    return true;
+}
---
+++ lib/TMC2209_Lib/TMC2209_Lib.hpp
@@ -0,0 +1,43 @@
+/*
+ * TMC2209_Lib.hpp
+ *
+ *  Created on: 13.07.2021
+ *      Author: AB
+ */
+
+#ifndef TMC2209_LIB_H_
+#define TMC2209_LIB_H_
+
+#include<Arduino.h>
+#include<stdint.h>
+#include<stdbool.h>
+
+#include"TMCSerial.hpp"
+#include"TMC22XX/TMC22XX_Map.hpp"
+
+
+
+class TMC2209 : private TMCSerial
+{
+    public:
+        /* Creates instance, sets up default configuration. */
+        TMC2209(HardwareSerial& serialPort, uint32_t baudrate, uint8_t chipAddress, uint8_t enablePin);
+
+        /* Checks driver is present, put driver in a disabled safe state for configuration. */
+        bool begin();
+
+        /* Check chip is alive :
+         *    - If chip is unconfigured, check version number
+         *    - If chip is configured, check error flag
+         */
+        bool isChipAlive();
+
+    private:
+        /* Driver enable pin */
+        uint8_t _enablePin;
+
+        /* Driver state and statuses */
+        bool _isConfigured;
+};
+
+#endif /* TMC2209_LIB_H_ */
