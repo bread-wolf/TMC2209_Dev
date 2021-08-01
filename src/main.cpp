1be23194ee29da037f9a84fbc3e0f7edf0bb3cfb
Augustin.B <augustinbielefeld@gmail.com>
2021-08-01 10:18:57 +0200

    main: add simple example code for tester board

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
+#include "TMC2209_Lib.hpp"
+
+#define CHIP_SERIAL  Serial2
+#define CHIP_BR      115200
+#define CHIP_ADD     0
+#define CHIP_EN      21
+TMC2209 TMC2209_Test(CHIP_SERIAL, CHIP_BR, CHIP_ADD, CHIP_EN);
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  if (!TMC2209_Test.begin())
+  {
+    // Turn on LED if chip isn't found.
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
